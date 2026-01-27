c4e40ae9da90755fba00399741eb038a48e1ce76
Denz Alec <denzeln3@outlook.com>
2026-01-27 02:35:11 +0000

    Debug: log channels data

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -49,6 +49,7 @@ export default function Community() {
     queryFn: async () => {
       const { data } = await supabase.from('channels').select('*, creator:users(id, display_name, avatar_url)').order('member_count', { ascending: false });
       return data || [];
+      console.log("Channels data:", data);
     }
   });
 
